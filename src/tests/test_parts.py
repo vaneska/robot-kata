43024da1379bcf251c172cad87699d70e1740f6b
Иван Соколов <vaneska@reg.ru>
2021-08-30 08:51:53 +0300

    Рефакторинг тестов

--- src/tests/test_parts.py
+++ src/tests/test_parts.py
@@ -3,28 +3,32 @@ from src.robot import parts
 
 class TestLeftArm:
 
-    def test_initial_charge(self):
-        arm = parts.LeftArm()
+    arm = parts.LeftArm()
 
-        assert arm._charge == 24
+    def test_initial_charge(self):
+        """
+        The left arm has a max charge (in Ampere Hour or Ah) of its internal 24KV battery.
+        """
+        assert self.arm._charge == 24
 
     def test_initial_turned_on(self):
-        arm = parts.LeftArm()
-
-        assert arm._turned_on_status is False
+        """
+        Finally the left arm also has a current On/Off switch status
+        """
+        assert self.arm._turned_on_status is False
 
 
 class TestRobotBus:
+    robot_bus = parts.RobotBus()
 
     def test_initial_turned_off(self):
-        robot_bus = parts.RobotBus()
-
-        assert robot_bus._turned_on_status is False
+        """
+        Robot bus has an On/Off status like the left arm
+        """
+        assert self.robot_bus._turned_on_status is False
 
     def test_left_arm_connected_to_robot_bus(self):
         """
         the left arm is connected to the robot bus.
         """
-        robot_bus = parts.RobotBus()
-
-        assert isinstance(robot_bus.left_arm, parts.LeftArm)
+        assert isinstance(self.robot_bus.left_arm, parts.LeftArm)
